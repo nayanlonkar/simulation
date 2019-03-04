53033a32ab8b8988d138e42947c29b18eba4f410
nayanlonkar <nayanlonkar@gmail.com>
2019-03-04 23:08:25 +0530

    program to generate histogram

---
+++ histogram.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+def histogram():
+    filename = input('Enter the file name > ')
+    noOfIntervals = int(input('Number of intervals > '))
+    fd = open(filename,'r')
+    data_array = [float(i.rstrip('\n')) for i in fd]
+    intervalsize = (max(data_array) - min(data_array))/noOfIntervals 
+    temp = min(data_array)
+    range_array = [temp]
+    max1 = max(data_array)
+    while(temp < max1):
+        temp += intervalsize
+        range_array.append(temp)
+
+    plt.hist(data_array, range_array)
+    plt.show()
+
+histogram()
