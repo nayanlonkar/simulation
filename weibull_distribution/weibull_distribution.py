e9eba19fc5028992705a94b92b3fc46fafe7b6bd
nayanlonkar <nayanlonkar@gmail.com>
2019-03-17 16:21:54 +0530

    added changes

--- weibull_distribution/weibull_distribution.py
+++ weibull_distribution/weibull_distribution.py
@@ -65,8 +65,9 @@ def beta_estimation(data_array):
     return beta
 
 
-
-#data_array = list(range(1,11))
-data_array = [70.292, 10.107, 48.386, 20.480, 13.053, 25.292, 14.713, 39.166, 17.421, 13.905]
+data_array = list(range(1,11))
+#data_array = [70.292, 10.107, 48.386, 20.480, 13.053, 25.292, 14.713, 39.166, 17.421, 13.905, 30.215, 17.137, 44.024, 10.552, 37.298, 16.314, 28.073, 39.019, 32.330, 36.547]
 beta = beta_estimation(data_array)
 print(beta)
+sq_data_array = [i**2 for i in data_array]
+print(sum(sq_data_array))
